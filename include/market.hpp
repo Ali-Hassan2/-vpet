be484208ac0cc67f6ac25c6fc8fc519a68461ec6
Ali Hassan <alihassan26032004@gmail.com>
2025-10-17 14:12:11 +0500

    modular-approach&owner-creation

---
+++ include/market.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <stirng>
+using namespace std;
+
+class market{
+    private:
+    string marketName;
+    string markedDescription;
+    string pets[];
+
+    public:
+    market(){
+        this->marketName = "";
+        this->markedDescription = "";
+        this.
+    }
+    void BuyPet(int petId) {};
+    void UpdatePetsData () {}; 
+}
